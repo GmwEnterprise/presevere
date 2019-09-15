827a65f974e73e17c5910e9a2dad377bc974a61a
GmwEnterprise <909498022@qq.com>
2019-09-15 23:34:18 +0800

    gmw

--- backstage/my-personal-website/src/main/java/cn/gmwenterprise/website/config/WebSecurityConfig.java
+++ backstage/my-personal-website/src/main/java/cn/gmwenterprise/website/config/WebSecurityConfig.java
@@ -62,13 +62,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-
+            .antMatchers("/login", "/error", "/logout").permitAll()
             .anyRequest().authenticated()
-
-            .and().formLogin().loginPage("/login").permitAll()
-            .defaultSuccessUrl("/success")
-
-            .and().csrf().disable();
+            .and()
+            .formLogin().loginPage("/login").defaultSuccessUrl("/success")
+            .and()
+            .logout().logoutUrl("/logout").logoutSuccessUrl("/login")
+            .and().httpBasic();
     }
 
     @Override
