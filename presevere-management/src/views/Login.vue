7af280017d92dbaf4a97f6e6ff35ef645007fc73
GmwEnterprise <909498022@qq.com>
2019-09-27 18:38:48 +0800

    gmw

--- presevere-management/src/views/Login.vue
+++ presevere-management/src/views/Login.vue
@@ -1,3 +1,20 @@
 <template>
   <div>login</div>
 </template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      redirectPath: '/sys'
+    }
+  },
+  created() {
+    const redirectPath = this.$route.query.redirect
+    if (redirectPath) {
+      this.redirectPath = redirectPath
+    }
+  }
+}
+</script>
