fb8f31ec43722a516b66b405ea0da01a39640d71
GmwEnterprise <909498022@qq.com>
2019-11-09 15:30:00 +0800

    gmw

--- presevere-ng/src/app/reading/components/results/results.component.ts
+++ presevere-ng/src/app/reading/components/results/results.component.ts
@@ -10,16 +10,16 @@ export class ResultsComponent implements OnInit {
 
   constructor(private route: ActivatedRoute) { }
 
-  ngOnInit() {
-    /**
-     * snapshot的问题在于，同组件路由切换无法更新路由参数信息
-     * /home/archives -> /home/2019/11 有信息
-     * /home/2019/11  -> /home/2019/9 无信息
-     */
-    const {
-      year, month
-    } = this.route.snapshot.params;
-    console.log(`${year || 'XXXX'}-${month || 'XX'}`);
+  year: string;
+  month: string;
+  get archive(): string {
+    return this.year + (this.month ? ` / ${this.month}` : '');
   }
 
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.year = params.get('year');
+      this.month = params.get('month');
+    });
+  }
 }
