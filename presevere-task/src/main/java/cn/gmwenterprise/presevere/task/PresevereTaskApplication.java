b2fd18566dfaeea234e31fe41da71e7e2bbbfaf6
GmwEnterprise <909498022@qq.com>
2019-11-25 23:51:20 +0800

    gmw

---
+++ presevere-task/src/main/java/cn/gmwenterprise/presevere/task/PresevereTaskApplication.java
@@ -0,0 +1,12 @@
+package cn.gmwenterprise.presevere.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresevereTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresevereTaskApplication.class, args);
+    }
+}
---
+++ presevere-task/src/test/java/cn/gmwenterprise/presevere/task/PresevereTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gmwenterprise.presevere.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresevereTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
