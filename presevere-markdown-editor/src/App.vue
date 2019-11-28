5588aeb112a2159fde2b5ca185ce8bfd01b93f71
GmwEnterprise <909498022@qq.com>
2019-11-29 01:10:43 +0800

    gmw

--- presevere-markdown-editor/src/App.vue
+++ presevere-markdown-editor/src/App.vue
@@ -1,58 +1,16 @@
 <template>
   <div id="app">
-    <div class="editor-box" :style="`width:${width}px`">
-      <div class="nav"></div>
-      <div class="editor-area" :style="`height:${height}px`">
-        <presevere-editor v-model="content" :style="`width: ${showRender ? '50' : '100'}%;${editorBorder}`"></presevere-editor>
-        <presevere-render :render="content" v-show="showRender"></presevere-render>
-      </div>
-    </div>
+    <markdown-editor></markdown-editor>
   </div>
 </template>
 
 <script>
-import PresevereEditor from '@/components/editor/TextareaEditor.vue'
-import PresevereRender from '@/components/editor/Render.vue'
+// import VersionOne from '@/components/Version1.vue'
+import MarkdownEditor from '@/components/editor/Editor.vue'
 export default {
   name: 'app',
   components: {
-    PresevereEditor,
-    PresevereRender
-  },
-  data() {
-    return {
-      content: 'hello',
-      showRender: true,
-      width: 1200,
-      height: 700
-    }
-  },
-  computed: {
-    editorBorder() {
-      return this.showRender ? 'border-right:2px solid #20a157' : ''
-    }
-  },
-  methods: {
-    inputE(e) {
-      console.log(e)
-    }
+    MarkdownEditor
   }
 }
 </script>
-
-<style scoped>
-.editor-area {
-  display: flex;
-  justify-content: space-between;
-  margin: 0 auto;
-  box-sizing: content-box;
-  border: 4px solid #20a157;
-}
-.nav {
-  width: 100%;
-  height: 50px;
-  border: 4px solid #20a157;
-  border-bottom: 0;
-  box-sizing: border-box;
-}
-</style>
---
+++ presevere-markdown-editor/src/components/Version1.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="version-one">
+    <div class="editor-box" :style="`width:${width}px`">
+      <div class="nav"></div>
+      <div class="editor-area" :style="`height:${height}px`">
+        <presevere-editor v-model="content" :style="`width: ${showRender ? '50' : '100'}%;${editorBorder}`"></presevere-editor>
+        <presevere-render :render="content" v-show="showRender"></presevere-render>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PresevereEditor from '@/components/editor/TextareaEditor.vue'
+import PresevereRender from '@/components/editor/Render.vue'
+export default {
+  name: 'VersionOne',
+  components: {
+    PresevereEditor,
+    PresevereRender
+  },
+  data() {
+    return {
+      content: 'hello',
+      showRender: true,
+      width: 1200,
+      height: 700
+    }
+  },
+  computed: {
+    editorBorder() {
+      return this.showRender ? 'border-right:2px solid #20a157' : ''
+    }
+  },
+  methods: {
+    inputE(e) {
+      console.log(e)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.editor-area {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  box-sizing: content-box;
+  border: 4px solid #20a157;
+}
+.nav {
+  width: 100%;
+  height: 50px;
+  border: 4px solid #20a157;
+  border-bottom: 0;
+  box-sizing: border-box;
+}
+</style>
