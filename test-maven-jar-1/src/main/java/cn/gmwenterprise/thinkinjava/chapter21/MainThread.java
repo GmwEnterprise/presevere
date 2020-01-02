d92e2c0fdbcddc36237287d1db9a08104fb67637
GmwEnterprise <909498022@qq.com>
2020-01-02 17:44:54 +0800

    gmw

---
+++ test-maven-jar-1/src/main/java/cn/gmwenterprise/thinkinjava/chapter21/MainThread.java
@@ -0,0 +1,10 @@
+package cn.gmwenterprise.thinkinjava.chapter21;
+
+public class MainThread {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            new Thread(new LiftOff()).start();
+        }
+        System.out.println("Waiting for LiftOff...");
+    }
+}
---
+++ test-maven-jar-1/src/main/java/cn/gmwenterprise/thinkinjava/chapter21/Practice1.java
@@ -0,0 +1,27 @@
+package cn.gmwenterprise.thinkinjava.chapter21;
+
+public class Practice1 implements Runnable {
+    private int count = 4;
+    private static int taskCount = 0;
+    private final int id = taskCount++;
+
+    public Practice1() {
+        System.out.printf("[%d]Start !%n", id);
+    }
+
+    @Override
+    public void run() {
+        while (count-- > 1) {
+            System.out.printf("[%d]count: %d%n", id, count);
+            Thread.yield();
+        }
+        System.out.printf("[%d]End !%n", id);
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            new Thread(new Practice1()).start();
+        }
+        System.out.println("----------------");
+    }
+}
