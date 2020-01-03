ce703edc7c7944b3a7ef01e3c236476a95eff415
GmwEnterprise <909498022@qq.com>
2020-01-03 17:28:25 +0800

    gmw

---
+++ test-maven-jar-1/src/main/java/cn/gmwenterprise/thinkinjava/chapter21/ThreadPool.java
@@ -0,0 +1,20 @@
+package cn.gmwenterprise.thinkinjava.chapter21;
+
+import java.util.concurrent.*;
+
+public class ThreadPool {
+
+    public void fixed() {
+        // Executors.newFixedThreadPool(5);
+        var exec = new ThreadPoolExecutor(5, 5, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+    }
+
+    public void cached() {
+        // Executors.newCachedThreadPool();
+        var exec = new ThreadPoolExecutor(0, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS, new SynchronousQueue<>());
+        for (int i = 0; i < 5; i++) {
+            exec.execute(new LiftOff());
+        }
+        exec.shutdown();
+    }
+}
