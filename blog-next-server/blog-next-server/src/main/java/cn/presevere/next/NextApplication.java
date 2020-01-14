fa5fd77898fe9e2c28aee122d26585c0c42d43ac
gmw mac os <gmw@gmwdeiMac.local>
2020-01-14 22:11:20 +0800

    gmw

--- blog-next-server/blog-next-server/src/main/java/cn/presevere/next/NextApplication.java
+++ blog-next-server/blog-next-server/src/main/java/cn/presevere/next/NextApplication.java
@@ -1,14 +1,13 @@
 package cn.presevere.next;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class NextApplication {
-    private static final Logger log = LoggerFactory.getLogger(NextApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(NextApplication.class, args);
     }
