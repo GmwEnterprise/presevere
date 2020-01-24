9968a41e8985bf9b7577f81ef8fb528362519b84
GmwEnterprise <909498022@qq.com>
2020-01-24 14:28:25 +0800

    gmw

--- blog-app/src/router/index.js
+++ blog-app/src/router/index.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    redirect: '/index'
-  }, {
-    path: '/index',
-    name: 'index',
-    component: () => import('../views/index/Index.vue')
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
