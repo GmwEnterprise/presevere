7d58f87cbed8dfbb858ec3d5bd4a1f05edba69da
GmwEnterprise <909498022@qq.com>
2020-01-28 04:14:31 +0800

    gmw

---
+++ demo/springcloud-start-one/src/main/java/com/example/microservice/springcloudstartone/SpringcloudStartOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice.springcloudstartone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudStartOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudStartOneApplication.class, args);
+    }
+
+}
---
+++ demo/springcloud-start-one/src/test/java/com/example/microservice/springcloudstartone/SpringcloudStartOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice.springcloudstartone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudStartOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
