3ed65a49637478408f46b26bc616058999828688
GmwEnterprise <909498022@qq.com>
2020-01-29 16:50:57 +0800

    gmw

--- demo/EagleEye/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ demo/EagleEye/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -1,13 +1,11 @@
-package com.example.microservice.springcloudstartone;
+package com.thoughtmechanix.licenses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringcloudStartOneApplication {
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudStartOneApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
