4f5d3cd65203e7a994e3a681d50efcebbb778303
GmwEnterprise <909498022@qq.com>
2020-02-03 20:22:51 +0800

    gmw

--- blog-next-springboot/src/main/java/cn/presevere/blog/App.java
+++ blog-next-springboot/src/main/java/cn/presevere/blog/App.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogNextSpringbootApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlogNextSpringbootApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 }
